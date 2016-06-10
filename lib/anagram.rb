94bb97c958b9ea1e0d587f20671558e7bc2c14bf
greatermeans <gakouri@gmail.com>
2016-06-09 20:42:59 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(anagram=nil)
+    @anagram = anagram
+  end
+
+  def match(words)
+    matching_words = []
+    words.each {|word|  matching_words << word if word.chars.sort == @anagram.chars.sort}
+    matching_words
+  end
+
+
+end
